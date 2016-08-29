34f56a6a5becabc461a5d0056d059784f7cebd40
ken1flan <ken1flan@gmail.com>
2016-08-30 07:39:34 +0900

    Add pattern image class

---
+++ classes/pattern_image.rb
@@ -0,0 +1,37 @@
+class PatternImage
+  require 'mini_magick'
+
+  attr_accessor :id
+
+  def initialize(id)
+    @id = id
+  end
+
+  def path
+    "images/patterns/#{id}.png"
+  end
+
+  def valid?
+    File.exist?(path)
+  end
+
+  def invalid?
+    !valid?
+  end
+
+  def image
+    if valid?
+      MiniMagick::Image.open(path)
+    else
+      nil
+    end
+  end
+
+  def self.valid?(id)
+    File.exist?("images/patterns/#{id}.png")
+  end
+
+  def self.invalid?(id)
+    !self.valid?(id)
+  end
+end
