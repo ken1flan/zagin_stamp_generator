b09724058386feba6e0fc9344ca51bc8304e0bfb
ken1flan <ken1flan@gmail.com>
2016-09-03 12:37:39 +0900

    Add component_image

--- classes/pattern_image.rb
+++ classes/pattern_image.rb
@@ -1,38 +1,9 @@
-class PatternImage
-  require 'mini_magick'
+require_relative 'component_image'
 
+class PatternImage < ComponentImage
   attr_accessor :id
 
-  def initialize(id)
-    @id = id
-  end
-
-  def path
-    image_dir = File.expand_path('../../images/patterns/', __FILE__)
-    "#{image_dir}/#{id}.png"
-  end
-
-  def valid?
-    File.exist?(path)
-  end
-
-  def invalid?
-    !valid?
-  end
-
-  def image
-    if valid?
-      MiniMagick::Image.open(path)
-    else
-      nil
-    end
-  end
-
-  def self.valid?(id)
-    File.exist?("images/patterns/#{id}.png")
-  end
-
-  def self.invalid?(id)
-    !self.valid?(id)
+  def self.image_dir
+    File.expand_path('../../images/patterns/', __FILE__)
   end
 end
